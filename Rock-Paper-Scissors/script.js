44d0869ef6e9f26fc24c7493fa08c3d26d78e44a
ymatevva <yoanamatevva@gmail.com>
2025-10-13 16:35:34 +0300

    Add simple mini project

---
+++ Rock-Paper-Scissors/script.js
@@ -0,0 +1,82 @@
+const maxChoiceExclusively = 3;
+
+function getComputerChoice() {
+
+   const choice = Math.floor(Math.random() * maxChoiceExclusively);
+
+    switch(choice) {
+        case 0:
+            return "rock";
+            break;
+        case 1:
+            return "scissors";
+            break;
+        case 2:
+            return "paper";
+            break;       
+    }
+}
+
+function getUserChoice() {
+    let input = prompt("Please, enter your choice (Rock, Scissors or Paper): ");
+    return input.toLowerCase();
+}
+
+function playRound(userChoice, opponentChoice) {
+     
+    if (userChoice === opponentChoice) {
+        return 0;
+    }
+
+   return (userChoice === "paper") 
+   ? (opponentChoice === "scissors" ? -1 : 1 ) 
+   : (userChoice === "scissors")  ? ( opponentChoice === "paper" ? 1 : -1 ) : ( opponentChoice === "paper" ? -1 : 1);
+}
+
+function playGame(roundsChoice) {
+   
+    let humanScore = 0;
+    let computerScore = 0;
+
+    for (let i = 0; i < roundsChoice; i++) {
+       
+       const userChoice = getUserChoice();
+       const computerChoice = getComputerChoice();
+
+       const roundResult = playRound(userChoice, computerChoice);
+
+       switch(roundResult) {
+        case 1: 
+             alert("Congrats! You won this round. Your opponent chose " + computerChoice.charAt(0).toUpperCase()  + computerChoice.slice(1).toLowerCase() + ".");
+             humanScore++;
+             break;
+        case -1:
+            alert("Sorry. You lost this round. Your opponent chose " + computerChoice.charAt(0).toUpperCase()  + computerChoice.slice(1).toLowerCase()+ ".");
+            computerScore++;
+            break;
+         case 0:
+            alert("No point for no one. You are equal.");        
+            break;
+       }
+    }
+
+    let scoreDiff = humanScore - computerScore;
+
+    switch(scoreDiff) {
+        case 0:
+           alert("Nobody wins. You are equal.");
+            break;
+        case (scoreDiff > 0):
+           alert("Congrats! You won!");
+            break;
+        case (scoreDiff < 0 ):
+           alert("Sorry. The opponent won.");
+        break;
+    }
+
+}
+
+
+alert("Welcome to the ROCK, PAPER, SCISSORS game!");
+let roundsChoice = Number(prompt("Please, enter how many rounds you want to play: "));
+playGame(roundsChoice);
